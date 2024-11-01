8a8f62f43a8e7fcfe3926fe80238cea8e99de4e5
verseler <handuman.verselerkerr@gmail.com>
2024-11-01 08:53:04 +0800

    feat: VerifiedUser page

---
+++ src/components/VerifiedUser/column.jsx
@@ -0,0 +1,83 @@
+import { ArrowUpDown } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import moment from "moment";
+
+export const columns = [
+  {
+    accessorKey: "id",
+    header: "ID",
+    cell: ({ row }) => {
+      return <div>{row.getValue("id")}</div>;
+    },
+  },
+  {
+    accessorKey: "email",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowUpDown className="w-4 h-4 ml-2" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <div>{row.getValue("email")}</div>;
+    },
+  },
+  {
+    accessorKey: "firstName",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          First Name
+          <ArrowUpDown className="w-4 h-4 ml-2" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <div>{row.getValue("firstName")}</div>;
+    },
+  },
+  {
+    accessorKey: "lastName",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Last Name
+          <ArrowUpDown className="w-4 h-4 ml-2" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <div>{row.getValue("lastName")}</div>;
+    },
+  },
+  {
+    accessorKey: "verifiedDate",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Verified Date
+          <ArrowUpDown className="w-4 h-4 ml-2" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const date = moment(row.getValue("verifiedDate")).format("YYYY-MM-DD");
+      return <div>{date}</div>;
+    },
+  },
+];
--- src/pages/VerifiedUserPage.jsx
+++ src/pages/VerifiedUserPage.jsx
@@ -1,9 +1,64 @@
 import Header from "@/components/Header/Header";
+import { columns } from "@/components/VerificationRequest/column";
+import { DataTable } from "@/components/DataTable/DataTable";
 
 export default function VerifiedUserPage() {
+  //!todo: change this to supabase fetch
+  const data = [
+    {
+      id: "728ed52f",
+      firstName: "John",
+      lastName: "Doe",
+      email: "a@example.com",
+      verifiedDate: "2023-07-25T00:00:00.000Z",
+    },
+    {
+      id: "42f51c34",
+      firstName: "Jane",
+      lastName: "Smith",
+      email: "b@example.com",
+      verifiedDate: "2022-01-15T00:00:00.000Z",
+    },
+
+    {
+      id: "91a23e45",
+      firstName: "Bob",
+      lastName: "Johnson",
+      email: "c@example.com",
+      verifiedDate: "2021-06-20T00:00:00.000Z",
+    },
+
+    {
+      id: "13d67e89",
+      firstName: "Alice",
+      lastName: "Williams",
+      email: "d@example.com",
+      verifiedDate: "2023-03-01T00:00:00.000Z",
+    },
+
+    {
+      id: "46b82c11",
+      firstName: "Mike",
+      lastName: "Davis",
+      email: "e@example.com",
+      verifiedDate: "2022-09-10T00:00:00.000Z",
+    },
+
+    {
+      id: "85f34a29",
+      firstName: "Emily",
+      lastName: "Taylor",
+      email: "f@example.com",
+      verifiedDate: "2021-11-25T00:00:00.000Z",
+    },
+  ];
+
   return (
     <div>
       <Header title="Verified User" />
+      <div className="px-4 py-10 mx-auto max-w-screen-2xl">
+        <DataTable columns={columns} data={data} />
+      </div>
     </div>
   );
 }
