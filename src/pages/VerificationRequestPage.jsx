1a35ab83efd0f677bd19cdc6b7033e9a29755560
verseler <handuman.verselerkerr@gmail.com>
2024-11-01 08:48:38 +0800

    feat: add VerificationRequest page

--- src/pages/VerificationRequestPage.jsx
+++ src/pages/VerificationRequestPage.jsx
@@ -1,9 +1,46 @@
 import Header from "@/components/Header/Header";
+import { columns } from "@/components/VerificationRequest/column";
+import { DataTable } from "@/components/DataTable/DataTable";
 
 export default function VerificationRequestPage() {
+  //!todo: change this to supabase fetch
+  const data = [
+    {
+      id: "728ed52f",
+      firstName: "John",
+      lastName: "Doe",
+      email: "a@example.com",
+      requestDate: "2023-07-25T00:00:00.000Z",
+    },
+    {
+      id: "728ed52f",
+      firstName: "Jake",
+      lastName: "Dave",
+      email: "jd@example.com",
+      requestDate: "2023-02-22T00:00:00.000Z",
+    },
+    {
+      id: "728e52f",
+      firstName: "Jane",
+      lastName: "Doe",
+      email: "j@example.com",
+      requestDate: "2023-07-15T00:00:00.000Z",
+    },
+    {
+      id: "728ed52f",
+      firstName: "Bob",
+      lastName: "Doe",
+      email: "b@example.com",
+      requestDate: "2023-07-10T00:00:00.000Z",
+    },
+  ];
+
   return (
     <div>
       <Header title="Verification Request" />
+      <div className="px-4 py-10 mx-auto max-w-screen-2xl">
+        <DataTable columns={columns} data={data} />
+      </div>
     </div>
   );
 }
