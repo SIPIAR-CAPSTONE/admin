06f2c767a42c1b17e8d68e821eddb570ab869c46
verseler <handuman.verselerkerr@gmail.com>
2024-11-05 00:58:57 +0800

    feat: login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,95 @@
+import AppLogo from "@/components/Sidebar/AppLogo";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { AtSign, Lock } from "lucide-react";
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6).max(50),
+});
+
 export default function LoginPage() {
-  return <div>login</div>;
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values) {
+    console.log(values);
+  }
+
+  return (
+    <div className="grid min-h-screen place-content-center dark:bg-neutral-900">
+      <Card className="w-80 dark:bg-neutral-800">
+        <CardHeader>
+          <AppLogo />
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="relative">
+                    <AtSign className="absolute size-4 top-3 left-2.5 text-neutral-400" />
+                    <FormControl>
+                      <Input
+                        placeholder="Email"
+                        type="email"
+                        className="h-10 pl-8 dark:border-neutral-700"
+                        autoComplete="email"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem className="relative">
+                    <Lock className="absolute size-4 top-3 left-2.5 text-neutral-400" />
+                    <FormControl>
+                      <Input
+                        placeholder="Password"
+                        type="password"
+                        className="h-10 pl-8 dark:border-neutral-700"
+                        autoComplete="current-password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                size="lg"
+                className="w-full bg-primary-500 hover:bg-primary-600 dark:bg-primary-500 dark:hover:bg-primary-600 dark:text-white"
+              >
+                Login
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
