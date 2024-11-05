07695edc8a11d297bcb0c70f0a39debb00b14a0b
verseler <handuman.verselerkerr@gmail.com>
2024-11-05 21:57:05 +0800

    style: adjust the badge size

--- src/components/User/column.jsx
+++ src/components/User/column.jsx
@@ -68,7 +68,7 @@ export const columns = [
       return (
         <Badge
           className={cn(
-            " rounded-xl",
+            "rounded-md",
             isVerified === "Yes"
               ? "bg-green-500 hover:bg-green-500 dark:bg-green-600 dark:text-white dark:hover:bg-green-600"
               : "bg-neutral-400 hover:bg-neutral-400 dark:bg-neutral-600 dark:text-white dark:hover:bg-neutral-600"
