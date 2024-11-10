2859268619ff323b118c87d0483d31e349c0cd32
verseler <handuman.verselerkerr@gmail.com>
2024-11-10 20:59:22 +0800

    style: change color from gray to neutral

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -7,22 +7,22 @@ export default function NotFoundPage() {
   //TODO: add authentication checking here
 
   return (
-    <section className="bg-white dark:bg-gray-900 ">
+    <section className="bg-white dark:bg-neutral-900 ">
       <div className="container flex items-center min-h-screen px-6 py-12 mx-auto">
         <div>
           <p className="text-sm font-medium text-blue-500 dark:text-blue-400">
             404 error
           </p>
-          <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">
+          <h1 className="mt-3 text-2xl font-semibold text-neutral-800 dark:text-white md:text-3xl">
             We can’t find that page
           </h1>
-          <p className="mt-4 text-gray-500 dark:text-gray-400">
+          <p className="mt-4 text-neutral-500 dark:text-neutral-400">
             Sorry, the page you are looking for doesn't exist or has been moved.
           </p>
 
           <div className="flex items-center mt-6 gap-x-3">
             <Button
-              className="space-x-2.5 dark:bg-gray-900 dark:text-white dark:border-gray-600"
+              className="space-x-2.5 dark:bg-neutral-900 dark:text-white dark:border-neutral-600"
               variant="outline"
               onClick={() => navigation(-1)}
             >
