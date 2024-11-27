b64b1420f353a6200c05aaa32cdf52a677967a2a
Amawers <dacoroon.christianjericho@gmail.com>
2024-11-27 22:04:06 +0800

    feat(verification request): added verification backend integration

--- src/pages/VerificationRequestPage.jsx
+++ src/pages/VerificationRequestPage.jsx
@@ -1,58 +1,77 @@
-import TopBar from "@/components/TopBar/TopBar";
-import { columns } from "@/components/VerificationRequest/column";
-import { DataTable } from "@/components/DataTable/DataTable";
+import TopBar from '@/components/TopBar/TopBar'
+import { columns } from '@/components/VerificationRequest/column'
+import { DataTable } from '@/components/DataTable/DataTable'
+import supabase from '@/supabase/config'
+import { useEffect, useState } from 'react'
 
-const data = {
-  breadcrumbs: [
-    {
-      name: "Verification Request",
-      href: "verification-request",
-    },
-  ],
-  tempTableData: [
-    {
-      id: "728ed52f",
-      firstName: "John",
-      lastName: "Doe",
-      email: "a@example.com",
-      requestDate: "2023-07-25T00:00:00.000Z",
-    },
-    {
-      id: "728ed52f",
-      firstName: "Jake",
-      lastName: "Dave",
-      email: "jd@example.com",
-      requestDate: "2023-02-22T00:00:00.000Z",
-    },
-    {
-      id: "728e52f",
-      firstName: "Jane",
-      lastName: "Doe",
-      email: "j@example.com",
-      requestDate: "2023-07-15T00:00:00.000Z",
-    },
-    {
-      id: "728ed52f",
-      firstName: "Bob",
-      lastName: "Doe",
-      email: "b@example.com",
-      requestDate: "2023-07-10T00:00:00.000Z",
-    },
-  ],
-};
+const breadCrumbs = [
+  { name: 'Verification Request', href: '/verification-request' },
+]
 
 export default function VerificationRequestPage() {
+  const [bystanderData, setBystanderData] = useState([])
+
+  const fetchBystanderData = async () => {
+    const { data, error } = await supabase.from('verification_request').select()
+
+    if (error) {
+      console.error('Error fetching bystander data:', error)
+    } else {
+      console.log('data verif page', data)
+
+      const formattedData = data.map((item) => ({
+        id: String(item.request_id),
+        bystanderId: String(item.bystander_id),
+        firstName: item.first_name,
+        middleName: item.middle_name,
+        lastName: item.last_name,
+        suffix: item.suffix,
+        birthDate: item.birth_date,
+        phoneNumber: item.phone_number,
+        city: 'Cagayan de Oro City',
+        barangay: item.barangay,
+        street: item.street,
+        houseNumber: item.house_number,
+        email: item.email,
+        request_date: item.request_date,
+      }))
+
+      setBystanderData(formattedData)
+    }
+  }
+
+  useEffect(() => {
+    fetchBystanderData()
+  }, [])
+
   return (
     <div>
-      <TopBar breadcrumbsData={data.breadcrumbs} />
+      <TopBar breadcrumbsData={breadCrumbs} />
       <div className="px-4 pb-4 mx-auto md:py-8 max-w-screen-2xl">
         <DataTable
           tableName="Verification Request"
           columns={columns}
-          data={data.tempTableData}
+          data={bystanderData}
           searchColumn="email"
+          func={fetchBystanderData}
+          statePropKeys={[
+            'id',
+            'bystanderId',
+            'firstName',
+            'middleName',
+            'lastName',
+            'suffix',
+            'birthDate',
+            'phoneNumber',
+            'city',
+            'barangay',
+            'street',
+            'houseNumber',
+            'email',
+            'request_date',
+          ]}
         />
       </div>
     </div>
-  );
+  )
 }
