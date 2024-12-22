0d9170d917a5529a9c5f20e51c3f3f430f34c511
Verseler kerr Handuman <121875436+Verseler@users.noreply.github.com>
2024-12-22 18:24:46 +0800

    style: add create and edit bystander form

--- src/pages/BystanderInfoPage.jsx
+++ src/pages/BystanderInfoPage.jsx
@@ -1,4 +1,9 @@
-import { BadgeCheck, CircleUserRound, MapPinHouse } from "lucide-react";
+import {
+  BadgeCheck,
+  CircleUserRound,
+  EllipsisVertical,
+  MapPinHouse,
+} from "lucide-react";
 
 import InfoCard from "@/components/InfoCard/InfoCard";
 import InfoCardField from "@/components/InfoCard/InfoCardField";
@@ -6,6 +11,18 @@ import TopBar from "@/components/TopBar/TopBar";
 import H1 from "@/components/ui/H1";
 import { useLocation } from "react-router-dom";
 import { getDate, getDateString } from "@/lib/utils";
+import { useAuth } from "@/context/AuthProvider";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import ConfirmationDialog from "@/components/ui/ConfirmationDialog";
+import { useState } from "react";
+import BystanderFormDialog from "@/components/Bystander/BystanderFormDialog";
+import { z } from "zod";
 
 const data = {
   breadcrumbs: [
@@ -20,19 +37,110 @@ const data = {
   ],
 };
 
+const editBystanderFormSchema = z.object({
+  firstName: z.string().min(1, { message: "First name is required" }),
+  middleName: z.string(),
+  lastName: z.string().min(1, { message: "Last name is required" }),
+  suffix: z.string(),
+  birthday: z.coerce.date(),
+  phoneNumber: z
+    .string()
+    .min(11, { message: "Phone number must be at least 11 characters" })
+    .regex(/^[0-9]+$/, { message: "Phone number must be numeric" })
+    .regex(/^(09)/, { message: "Phone number must start with 09" }),
+  barangay: z.string().min(1, { message: "Barangay is required" }),
+  street: z.string().min(1, { message: "Street is required" }),
+  houseNumber: z.string(),
+});
+
 export default function BystanderInfoPage() {
   const { state } = useLocation();
-  const { firstName, middleName, lastName, suffix, birthDate, phoneNumber, barangay, street, houseNumber, isVerified, verifiedDate } = state;
-
+  const {
+    id,
+    firstName,
+    middleName,
+    lastName,
+    suffix,
+    birthDate,
+    phoneNumber,
+    barangay,
+    street,
+    houseNumber,
+    isVerified,
+    verifiedDate,
+  } = state;
   const birthDateParse = getDate(birthDate);
   const verificationDate = getDateString(verifiedDate);
+  const { role } = useAuth();
+  const [loading, setLoading] = useState(false);
+
+  const [deleteDialogIsOpen, setDeleteDialogIsOpen] = useState(false);
+  const openDeleteDialog = () => setDeleteDialogIsOpen(true);
+
+  const [formIsOpen, setFormIsOpen] = useState(false);
+  const openFormDialog = () => setFormIsOpen(true);
+
+  const editBystanderAccount = async (values) => {
+    try {
+      setLoading(true);
+
+      //TODO: query
+      console.log(values);
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleDeleteBystander = async () => {
+    console.log("delete bystander");
+  };
 
   return (
     <>
       <TopBar breadcrumbsData={data.breadcrumbs} addBackButton />
       <div className="max-w-5xl px-4 py-8 mx-auto">
-        <H1>Bystander Info</H1>
-
+        <div className="flex items-center justify-between gap-2 pb-6">
+          <H1>Bystander Info</H1>
+          {role === "verifier" && (
+            <Menubar className="p-0 border-none shadow-none dark:bg-transparent">
+              <MenubarMenu>
+                <MenubarTrigger className="py-1 cursor-pointer px-1.5 dark:bg-transparent hover:bg-neutral-100 transition-colors dark:text-white hover:dark:bg-neutral-700">
+                  <EllipsisVertical />
+                </MenubarTrigger>
+                <MenubarContent className="dark:bg-neutral-700">
+                  <MenubarItem onClick={openFormDialog}>Edit</MenubarItem>
+                  <MenubarItem
+                    className="text-red-500"
+                    onClick={openDeleteDialog}
+                  >
+                    Delete
+                  </MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+            </Menubar>
+          )}
+        </div>
+        <BystanderFormDialog
+          title="Edit Bystander Information"
+          open={formIsOpen}
+          setOpen={setFormIsOpen}
+          onSubmit={editBystanderAccount}
+          loading={loading}
+          defaultValues={{
+            firstName: firstName,
+            middleName: middleName,
+            lastName: lastName,
+            suffix: suffix,
+            birthday: birthDate,
+            phoneNumber: phoneNumber,
+            barangay: barangay,
+            street: street,
+            houseNumber: houseNumber,
+          }}
+          formSchema={editBystanderFormSchema}
+        />
         <div className="grid gap-6 pt-6 md:grid-cols-2">
           <InfoCard LabelIcon={BadgeCheck} label="Verification Status">
             <InfoCardField
@@ -61,6 +169,15 @@ export default function BystanderInfoPage() {
             <InfoCardField label="House Number" value={houseNumber} />
           </InfoCard>
         </div>
+
+        <ConfirmationDialog
+          isOpen={deleteDialogIsOpen}
+          setOpen={setDeleteDialogIsOpen}
+          title="Delete Bystander Account"
+          description="This action cannot be undone. Are you sure you want to delete this bystander?"
+          onConfirm={handleDeleteBystander}
+          variant="destructive"
+        />
       </div>
     </>
   );
--- src/pages/BystandersPage.jsx
+++ src/pages/BystandersPage.jsx
@@ -1,9 +1,12 @@
 import TopBar from "@/components/TopBar/TopBar";
 import { columns } from "@/components/Bystander/column";
 import { DataTable } from "@/components/DataTable/DataTable";
-import { Badge, BadgeCheck } from "lucide-react";
+import { Badge, BadgeCheck, Plus } from "lucide-react";
 import supabase from "@/supabase/config";
-import { useEffect, useState } from "react";
+import BystanderFormDialog from "@/components/Bystander/BystanderFormDialog";
+import { Button } from "@/components/ui/button";
+import { useState, useEffect } from "react";
+import { z } from "zod";
 
 const breadCrumbs = [{ name: "Bystanders", href: "/bystander" }];
 
@@ -22,9 +25,44 @@ const filters = {
   ],
 };
 
+const createBystanderFormSchema = z.object({
+  firstName: z.string().min(1, { message: "First name is required" }),
+  middleName: z.string(),
+  lastName: z.string().min(1, { message: "Last name is required" }),
+  suffix: z.string(),
+  birthday: z.coerce.date(),
+  phoneNumber: z
+    .string()
+    .min(11, { message: "Phone number must be at least 11 characters" })
+    .regex(/^[0-9]+$/, { message: "Phone number must be numeric" })
+    .regex(/^(09)/, { message: "Phone number must start with 09" }),
+  barangay: z.string().min(1, { message: "Barangay is required" }),
+  street: z.string().min(1, { message: "Street is required" }),
+  houseNumber: z.string(),
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters" })
+    .regex(/[A-Z]/, {
+      message: "Password must contain at least one uppercase letter",
+    })
+    .regex(/[a-z]/, {
+      message: "Password must contain at least one lowercase letter",
+    })
+    .regex(/[0-9]/, { message: "Password must contain at least one number" })
+    .regex(/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+/, {
+      message: "Password must contain at least one special character",
+    })
+    .regex(/\d/, { message: "Password must contain at least one number" })
+    .max(35, { message: "Password must not exceed 35 characters" }),
+});
+
 export default function BystandersPage() {
   const [bystanderData, setBystanderData] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [createBystanderLoading, setCreateBystanderLoading] = useState(false);
+  const [formIsOpen, setFormIsOpen] = useState(false);
+  const openFormDialog = () => setFormIsOpen(true);
 
   const fetchBystanderData = async () => {
     try {
@@ -39,10 +77,10 @@ export default function BystandersPage() {
         )
       `);
       if (error) {
-        console.error("Error fetching bystander data:", error);
-      } else {
+        console.error("Error fetching bystander data:", error.message);
+      }
 
-
+      if (data) {
         const formattedData = data.map((item) => ({
           id: String(item.bystander_id),
           firstName: item.USER.first_name,
@@ -74,9 +112,59 @@ export default function BystandersPage() {
     fetchBystanderData();
   }, []);
 
+  function createBystanderButton() {
+    return (
+      <Button
+        size="sm"
+        onClick={openFormDialog}
+        className="bg-green-500 me-2 dark:text-white hover:bg-green-600 cursor-pointer dark:bg-green-600 dark:hover:bg-green-500"
+      >
+        <Plus className="text-xl" />
+        Bystander
+      </Button>
+    );
+  }
+
+  const createBystanderAccount = async (values) => {
+    try {
+      setCreateBystanderLoading(true);
+
+      //TODO: query
+      console.log(values);
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setCreateBystanderLoading(false);
+    }
+  };
+
   return (
     <div>
-      <TopBar breadcrumbsData={breadCrumbs} />
+      <TopBar
+        breadcrumbsData={breadCrumbs}
+        renderTrailer={createBystanderButton}
+      />
+      <BystanderFormDialog
+        title="Create New Bystander"
+        open={formIsOpen}
+        setOpen={setFormIsOpen}
+        onSubmit={createBystanderAccount}
+        loading={createBystanderLoading}
+        defaultValues={{
+          firstName: "",
+          middleName: "",
+          lastName: "",
+          suffix: "",
+          birthday: "",
+          phoneNumber: "",
+          barangay: "",
+          street: "",
+          houseNumber: "",
+          email: "",
+          password: "",
+        }}
+        formSchema={createBystanderFormSchema}
+      />
       <div className="px-4 pb-4 mx-auto md:py-8 max-w-screen-2xl">
         <DataTable
           loading={loading}
